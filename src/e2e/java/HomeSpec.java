0f4677b31550df22a1873d9490fed2b262378dae
PetroRavlinko <petro.ravlinko@gmail.com>
2016-09-28 13:15:49 +0300

    Add e2e tests

---
+++ src/e2e/java/HomeSpec.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HomeSpec {
+	@Test
+	public void call_home_services_endpoint() {
+		given().baseUri("http://localhost:80/services").
+				when().get("/").
+				then().statusCode(200).
+				and().body("message", equalTo("Hello!"));
+
+	}
+}
